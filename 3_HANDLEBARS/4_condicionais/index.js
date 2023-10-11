398fe1dbc3df058f243caaa7ddd6bbe2429f3869
Maedes Chaves <maedespf@gmail.com>
2023-10-10 22:18:45 -0300

    repeticao

---
+++ 3_HANDLEBARS/4_condicionais/index.js
@@ -0,0 +1,27 @@
+const express = require("express")
+const exphbs = require("express-handlebars")
+
+const app = express()
+
+// configurando o handlebars como template engine
+app.engine('handlebars', exphbs.engine())
+app.set('view engine', 'handlebars')
+
+app.get('/', (req, res) => {
+    res.render('dashboard')
+})
+
+app.get('/', (req, res) => {
+    const usuario  = {
+        nome: "Maedes",
+        profissao: "Desenvolvedor Web",
+        idade: 20
+    }
+const auth = false
+
+    res.render('home', { usuario, auth })
+})
+
+app.listen(3000, () => {
+    console.log("Servidor rodando na porta 3000!")
+})
---
+++ 3_HANDLEBARS/5_repeticao/index.js
@@ -0,0 +1,29 @@
+const express = require("express")
+const exphbs = require("express-handlebars")
+
+const app = express()
+
+// configurando o handlebars como template engine
+app.engine('handlebars', exphbs.engine())
+app.set('view engine', 'handlebars')
+
+app.get('/', (req, res) => {
+const itens = ['itens a', 'itens b', 'itens c']
+
+    res.render('dashboard', { itens })
+})
+
+app.get('/', (req, res) => {
+    const usuario  = {
+        nome: "Maedes",
+        profissao: "Desenvolvedor Web",
+        idade: 20
+    }
+const auth = false
+
+    res.render('home', { usuario, auth })
+})
+
+app.listen(3000, () => {
+    console.log("Servidor rodando na porta 3000!")
+})
